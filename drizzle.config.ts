672e3c3af244c875120dbb4192e066da0538aaf1
Galym Nus <justtairex@gmail.com>
2025-12-23 17:56:56 +0000

    update drizzle.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "drizzle-kit";
-import { readConfig } from './src/config';
+import { readConfig } from './src/config'
 
+const url = readConfig().dbUrl;
 
 export default defineConfig({
   schema: "src/lib/db",
   out: "src/lib/db/out",
   dialect: "postgresql",
   dbCredentials: {
-    url: readConfig().dbUrl
+    url: url
   },
 });
 
