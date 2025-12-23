2655cad73e00d7d89765628e1843ddcd57d3a609
Galym Nus <justtairex@gmail.com>
2025-12-23 17:58:58 +0000

    add commands

--- src/CommandHandler.ts
+++ src/CommandHandler.ts
@@ -1,25 +1,98 @@
-import { setUser } from './config';
+import { setUser, readConfig } from './config';
+import { createUser, getUserByName, resetUsers, getUsers, getUserById } from './lib/db/queries/users';
+import { createFeed, getFeeds } from './lib/db/queries/feeds';
+import { fetchFeed } from "./RSSfeed";
 
-type CommandHandler = (cmd: string, ...args: string[]) => void;
+type CommandHandler = (cmd: string, ...args: string[]) => Promise<void>;
 export type CommandsRegistry = Record<string, CommandHandler>
 
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
     if (args.length < 1) {
         console.log("login command requires username as parameter!");
         process.exit(1);
     } else {
         const username = args[0];
-        setUser(username);
-        console.log(`User has been set to ${username}`)
+        const is_registered = await getUserByName(username);
+        if (is_registered) {
+            setUser(username);
+            console.log(`User has been set to ${username}`)
+        } else {
+            console.log(`User: ${username} not found!`);
+            process.exit(1);
+        }
+    }
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+    if (args.length < 1) {
+        console.log("Register command requires name as parameter!");
+        process.exit(1);
+    } else {
+        const name = args[0];
+        await createUser(name);
+        setUser(name);
+        console.log(`User ${name} has been registered.`)
     }
 }
 
 
-export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler) {
+export async function handlerResetUsers(cmdName: string, ...args: string[]) {
+    const is_success = await resetUsers()
+    if (is_success) {
+        setUser("");
+        console.log(`Users table was reset.`)
+        process.exit(0);
+    } else {
+        console.log(`Users table was not reset.`)
+        process.exit(1);
+    }
+}
+
+export async function handlerGetUsers(cmdName: string) {
+    const users = await getUsers();
+    const currentUser = readConfig().currentUserName;
+    for (const user in users) {
+        const username = users[user].name;
+        let add = "";
+        if (username == currentUser) {
+            add = " (current)";
+        }
+        console.log(`* ${username}${add}`);
+    }
+}
+
+export async function handlerAggregation(cmdName: string, ...args: string[]) {
+    const text = await fetchFeed("https://www.wagslane.dev/index.xml");
+    console.log(text);
+}
+
+export async function handlerCreateFeed(cmdName: string, ...args: string[]) {
+    if (args.length < 2) {
+        console.log("addfeed command requires name and url as parameters!");
+        process.exit(1);
+    } else {
+        const currentUserName = readConfig().currentUserName;
+        const user = await getUserByName(currentUserName);
+        const text = await createFeed(args[0], args[1], user.id);
+    }
+}
+
+export async function handlerGetFeeds(cmdName: string, ...args: string[]) {
+    const feeds = await getFeeds();
+    for (let feed in feeds) {
+        const { name, url, userId } = feeds[feed];
+        const user = await getUserById(userId);
+        console.log(`${feed + 1}| ${name} from ${url} by ${user.name}`);
+    }
+}
+
+
+
+export async function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler) {
     registry[cmdName] = handler;
 };
 
-export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
-    registry[cmdName](cmdName, ...args);
+export async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
+    await registry[cmdName](cmdName, ...args);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-import { CommandsRegistry, registerCommand, handlerLogin, runCommand } from "./CommandHandler";
+import { CommandsRegistry, registerCommand, handlerLogin, runCommand, handlerCreateFeed, handlerResetUsers, handlerGetUsers, handlerAggregation, handlerRegister, handlerGetFeeds } from "./CommandHandler";
 
-function main() {
+async function main() {
     const commandRegistry: CommandsRegistry = {};
     registerCommand(commandRegistry, "login", handlerLogin);
+    registerCommand(commandRegistry, "register", handlerRegister);
+    registerCommand(commandRegistry, "reset", handlerResetUsers);
+    registerCommand(commandRegistry, "users", handlerGetUsers);
+    registerCommand(commandRegistry, "agg", handlerAggregation);
+    registerCommand(commandRegistry, "addfeed", handlerCreateFeed);
+    registerCommand(commandRegistry, "feeds", handlerGetFeeds);
     const command = process.argv[2];
     if (!command) {
         console.log("Error: not enough arguments were provided");
         process.exit(1);
     } else {
         if (commandRegistry[command]) {
-            runCommand(commandRegistry, command, ...process.argv.slice(3));
+            await runCommand(commandRegistry, command, ...process.argv.slice(3));
         } else {
             console.log("Error: not found command ", command);
             process.exit(1);
         }
     }
+    process.exit(0);
 }
 
 main();
